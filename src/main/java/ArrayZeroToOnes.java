e9cbbed0a89bfede759c47e156c1b3db6b505a6f
Amer <amer.rasheed@gmail.com>
2021-02-05 11:12:37 +0100

    5 Feb

--- src/main/java/ArrayZeroToOnes.java
+++ src/main/java/ArrayZeroToOnes.java
@@ -2,7 +2,7 @@ public class ArrayZeroToOnes {
 
     public int[] setMethod(int[] testArray) {
         for( int i=0;i<testArray.length;i++)
-        {
+         {
             if (testArray[i]==0)
                 testArray[i]=1;
         }
